339c4a2468b916b90ec662365af37d9c3cab7d74
SukhanRumanov <romas3.11.05@mail.ru>
2025-10-27 13:40:59 +0500

    финал

--- params.py
+++ params.py
@@ -3,7 +3,7 @@ import os
 db_params = {
     'dbname': os.getenv('DB_NAME', 'news_db'),
     'user': os.getenv('DB_USER', 'postgres'),
-    'password': os.getenv('DB_PASSWORD', '123456'),
+    'password': os.getenv('DB_PASSWORD', 'пароль'),
     'host': os.getenv('DB_HOST', 'host.docker.internal'),
     'port': os.getenv('DB_PORT', '5432')
 }
